e844be7ac640c70aae9effb6afb12025e7594ec2
dusanrsc <dusanrosic25.06.1997@gmail.com>
2025-07-04 23:21:01 +0200

    Minor Refactorization Done.

--- app/src/main/java/com/infinitysoftware/notes/App.kt
+++ app/src/main/java/com/infinitysoftware/notes/App.kt
@@ -1,8 +1,10 @@
 package com.infinitysoftware.frontendwithdatabaseapp
 
+import android.content.Context.MODE_PRIVATE
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.*
@@ -10,60 +12,73 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.infinitysoftware.notes.ui.theme.DefaultBackgroundColor
 
+import com.infinitysoftware.notes.ui.theme.DefaultBackgroundColor
+import com.infinitysoftware.notes.ui.theme.DefaultFontColor
+import com.infinitysoftware.notes.ui.theme.DefaultLineColor
+import java.io.OutputStreamWriter
+
+// Composable Application Function.
 @Composable
 fun App() {
+
+    // Application Settings Section
+    // Custom Variable/Constants Section
     val configuration = LocalConfiguration.current
     val screenHeight = configuration.screenHeightDp
 
-    val lineSpacingDp = 50
-    val numberOfLines = screenHeight / lineSpacingDp
+    val defaultFontSize = 30
+    val lineSpacing = 34
+    val numberOfLines = screenHeight / lineSpacing
 
+    // Remember Variable/Constants Section.
     var enteredText by remember { mutableStateOf("") }
 
-    Box(
-        modifier = Modifier
+    // Application UI/GUI Section.
+    Box(modifier = Modifier
             .fillMaxSize()
             .background(DefaultBackgroundColor)
             .padding(horizontal = 16.dp, vertical = 24.dp)
     ) {
+        // USING THIS LOC ONLY IN DEVELOPMENT STAGE.
+        // Text(text = "${numberOfLines}", color = Color.Green)
+
         Column(
             modifier = Modifier.fillMaxSize()
         ) {
-            Spacer(modifier = Modifier.height(lineSpacingDp.dp)) // pomera prvu liniju nadole
+            Spacer(modifier = Modifier.height((lineSpacing + 11).dp))
 
+            // Looping Through Screen Size (Render As Many Lines AS Needed By Screen Size)
             for (i in 0..numberOfLines) {
                 Divider(
-                    color = Color.DarkGray,
+                    color = DefaultLineColor,
                     thickness = 1.dp,
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp)
                 )
-                Spacer(modifier = Modifier.height(lineSpacingDp.dp))
+                Spacer(modifier = Modifier.height(lineSpacing.dp))
             }
         }
         TextField(
             value = enteredText,
             onValueChange = { enteredText = it},
-            textStyle = TextStyle(fontSize = numberOfLines.sp),
+            textStyle = TextStyle(
+                fontSize = defaultFontSize.sp,
+                fontFamily = FontFamily.Default,
+                color = DefaultFontColor),
+
             colors = TextFieldDefaults.colors(
-                focusedTextColor = Color.Black,
+                focusedTextColor = DefaultFontColor,
                 unfocusedTextColor = Color.Transparent,
                 cursorColor = Color.Transparent,
                 focusedContainerColor = Color.Transparent,
                 unfocusedContainerColor = Color.Transparent,
                 focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent))
+                unfocusedIndicatorColor = Color.Transparent)
+        )
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun AppPreviewer() {
-    App()
 }
--- app/src/main/java/com/infinitysoftware/notes/ui/theme/Color.kt
+++ app/src/main/java/com/infinitysoftware/notes/ui/theme/Color.kt
@@ -10,4 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val DefaultBackgroundColor = Color(0xFFFFC600)
+val DefaultBackgroundColor = Color(0xFFFFDC8D)
+val DefaultLineColor = Color(0xFF344200)
+
+val DefaultFontColor = Color(0xFF2E5DAF)
